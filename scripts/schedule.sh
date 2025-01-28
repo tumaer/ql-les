2a9f740b4d52b41e780a8c73c67d9c0525a4a35b
Artur Toshev <artur.toshev@tum.de>
2025-01-28 21:16:33 +0100

    make mlp_num_layers=1 for lagrangebench setup

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -5,5 +5,5 @@
 python src/train.py trainer.devices=[0] experiment=gns_train_eval_2d_kolm.yaml \
   +logger.wandb.name=koml2d model.alpha_u=1.0 model.vel_solver=simple
 
-# python src/train.py trainer.devices=[0] experiment=gns_train_eval_2d_kolm.yaml \
-#   +logger.wandb.name=koml2d model.alpha_u=0.0000001
+python src/train.py trainer.devices=[0] experiment=gns_train_eval_2d_kolm.yaml \
+  +logger.wandb.name=koml2d model.alpha_u=0.0000001 model.vel_solver=simple
