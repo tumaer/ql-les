32999727485136456bfc72aa99b7b06e14533d64
komnin42 <teodor4.kalinov@gmail.com>
2025-02-06 20:19:24 +0100

    Minor bug fix in the training script

--- src/train.py
+++ src/train.py
@@ -82,6 +82,10 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         "logger": logger,
         "trainer": trainer,
     }
+    
+    if logger:
+        log.info("Logging hyperparameters!")
+        log_hyperparameters(object_dict)
 
     if cfg.get("train"):
         log.info("Starting training!")
