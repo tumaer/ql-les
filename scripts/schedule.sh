53ed634d45ac36139ad6d7e32993de553c4d2d22
arturtoshev <artur.toshev@tum.de>
2026-04-26 01:01:34 +0200

    cleanup

--- scripts/schedule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Schedule execution of many runs
-# nohup bash scripts/schedule.sh > gns_kolm2d_every1_simple.log 2>&1 &
-# or
-# sbatch slurm_schedule.sh
-
-python src/train.py experiment=gns_kolm2d_every1.yaml \
-  +logger.wandb.name=gns_simple model.alpha_u=1 model.vel_solver=simple
